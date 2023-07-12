8ce5aaebf1bfa7e376726681f8154650a5821ef3
Kevin <kevinospinal12@gmail.com>
2023-07-12 01:18:17 -0500

    codigo final

--- main.js
+++ main.js
@@ -1,141 +1,119 @@
-const NavEmail = document.querySelector('.navbar-email');
+const menuEmail = document.querySelector('.navbar-email');
+const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuBurgerIcon = document.querySelector('.menu');
-const movileMenu = document.querySelector('.mobile-menu');
-const menuCarrito = document.querySelector('.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
-const cardContainer = document.querySelector('.cards-container')
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
+const productDetailContainer = document.querySelector('#productDetail');
+const cardsContainer = document.querySelector('.cards-container');
 
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
-NavEmail.addEventListener('click', toggleMenu)
+function toggleDesktopMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-menuBurgerIcon.addEventListener('click', toggleMovileMenu)
-
-menuCarrito.addEventListener('click',toggleCarritoAside)
-
-
-
-function toggleMenu(){
-    const isAsideCloce = aside.classList.contains('inactive')
-
-    if(!isAsideCloce){
-        aside.classList.add('inactive')
-    }
-
-    movileMenu.classList.toggle('inactive')
-    desktopMenu.classList.toggle('inactive')
-
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive');
+  }
+  
+  desktopMenu.classList.toggle('inactive');
 }
 
-function toggleMovileMenu(){
-    const isAsideCloce = aside.classList.contains('inactive')
+function toggleMobileMenu() {
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
-    if(!isAsideCloce){
-        aside.classList.add('inactive')
+  if (!isAsideClosed) {
+    shoppingCartContainer.classList.add('inactive'); 
+  }
 
-    }
-
-    movileMenu.classList.toggle('inactive')
+  closeProductDetailAside();
+  
+  mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside(){
-    const isMovileMenuClose = movileMenu.classList.contains('inactive')
-    const isDesktop = desktopMenu.classList.contains('inactive')
-    if(!isMovileMenuClose){
-            movileMenu.classList.add('inactive');
-    }
-    if(!isDesktop){
-        desktopMenu.classList.add('inactive')
-    }
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+  }
 
-    aside.classList.toggle('inactive')
-
+  const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+  
+  if (!isProductDetailClosed) {
+    productDetailContainer.classList.add('inactive'); 
+  }
+  
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
-
-
-const productoList = []
-productoList.push({
-    name:"Camisas overzait",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-productoList.push({
-    name:"Camisillas overzit",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-productoList.push({
-    name:"Camisillas overzit",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-productoList.push({
-    name:"Camisas overzait",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-productoList.push({
-    name:"Camisillas overzit",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-productoList.push({
-    name:"Camisas overzait",
-    price:"$120",
-    img:"https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226",
-})
-/*
-<div class="product-card">
-        <img src="https://dynamobrand.co/cdn/shop/products/DSC00499.jpg?v=1681252226" alt="">
-        <div class="product-info">
-          <div>
-            <p>$120,00</p>
-            <p>Bike</p>
-          </div>
-          <figure>
-            <img src="./icons/bt_add_to_cart.svg" alt="">
-          </figure>
-        </div>
-      </div>
-*/
-function renderProduct(arr){
-    for(product of productoList){
-        const productCard = document.createElement('div')
-        productCard.classList.add('product-card')
-    
-        //product = {name,price,imagen} product.image
-        const ProductImg = document.createElement('img')
-        ProductImg.setAttribute('src',product.img)
-        //
-        const productInfo = document.createElement('div')
-        productInfo.classList.add('product-info')
-    
-        const productInfoDIV = document.createElement('div')
-    
-        const productPrice = document.createElement('p')
-        productPrice.innerText = product.price
-    
-        const productName = document.createElement('p')
-        productName.innerText = product.name;
-    
-        productInfoDIV.appendChild(productPrice)
-        productInfoDIV.appendChild(productName)
-    
-        const productInfoFigure = document.createElement('figure')
-        const productImgCart = document.createElement('img')
-        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
-    
-        productInfoFigure.appendChild(productImgCart)
-    
-        productInfo.appendChild(productInfoDIV);
-        productInfo.appendChild(productInfoFigure);
-    
-        productCard.appendChild(ProductImg);
-        productCard.appendChild(productInfo);
-    
-    
-        cardContainer.appendChild(productCard)
-    }
+function openProductDetailAside() {
+  shoppingCartContainer.classList.add('inactive');
+  productDetailContainer.classList.remove('inactive');
 }
-renderProduct(productoList);
+
+function closeProductDetailAside() {
+  productDetailContainer.classList.add('inactive');
+}
+
+const productList = [];
+productList.push({
+  name: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Pantalla',
+  price: 220,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+  name: 'Compu',
+  price: 620,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+function renderProducts(arr) {
+  for (product of arr) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+  
+    // product= {name, price, image} -> product.image
+    const productImg = document.createElement('img');
+    productImg.setAttribute('src', product.image);
+    productImg.addEventListener('click', openProductDetailAside);
+  
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+  
+    const productInfoDiv = document.createElement('div');
+  
+    const productPrice = document.createElement('p');
+    productPrice.innerText = '$' + product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+  
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+  
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+  
+    productInfoFigure.appendChild(productImgCart);
+  
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+  
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+  
+    cardsContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
